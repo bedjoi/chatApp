373da6fa62d584499c9d8bf5f680e654c63acd8d
obedi bedjo <obedindevu@gmail.com>
2022-11-16 09:44:41 +0200

    feat:setup errorHandlers

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// setup error handlers
+
+const errorHandlers = require('./handlers/errorHandlers');
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongoseErrors);
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    
+    app.use(errorHandlers.developmentErrors);
+}else{
+    app.use(errorHandlers.productionErrors);
+}
+
 module.exports = app();
