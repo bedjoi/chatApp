4e5c5f6ddca718dfd54f5baecbccd8ea2eda915b
obedi bedjo <obedindevu@gmail.com>
2022-11-18 22:06:43 +0200

    fix: authentication

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express= require('express');
+const express = require("express");
+
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//brinn in the routes
-app.use("/user",require("./routes/user"));
+//Setup Cross Origin
+app.use(require("cors")());
 
+//Bring in the routes
+app.use("/user", require("./routes/user"));
+app.use("/chatroom", require("./routes/chatroom"));
 
-// setup error handlers
+//Setup Error Handlers
 
-const errorHandlers = require('./handlers/errorHandlers');
+const errorHandlers = require("./handlers/errorHandlers");
 app.use(errorHandlers.notFound);
 app.use(errorHandlers.mongoseErrors);
-if (process.env.ENV === 'DEVELOPMENT') {
-    
-    app.use(errorHandlers.developmentErrors);
-}else{
-    app.use(errorHandlers.productionErrors);
+if (process.env.ENV === "DEVELOPMENT") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
 }
+
 
 module.exports = app;
--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -72,4 +72,4 @@ exports.catchErrors = (fn) => {
       message: "Route not found",
     });
   };
-  
+ // 
