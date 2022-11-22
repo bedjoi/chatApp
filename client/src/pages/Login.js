05aacacb3cc1f8333f28c8222c63f4b901f6ed03
obedi bedjo <obedindevu@gmail.com>
2022-11-22 12:19:05 +0200

    style: login style

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function LoginPage() {
   return (
-    <div>Login page</div>
+    <div className='App'>
+        <div className='card'>
+            <p className='login'>Login</p>
+            <div className="inputLogin">
+                <label htmlFor="email">Email</label>
+                <input type='text' Name="email" id="inputMail" placeholder='your mail'></input>
+                <label htmlFor="email">password</label>
+                <input type='text' Name="name" id="inputName" placeholder='your password'></input>
+            <button>Submit</button>
+            </div>  
+        </div>
+
+
+    </div>
   )
 }
