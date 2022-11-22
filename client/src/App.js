bb0cf6ec52a14588d5b1c64f7c217fedf129aef5
obedi bedjo <obedindevu@gmail.com>
2022-11-22 10:45:03 +0200

    feat : create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import LoginPage from './pages/Login';
+import RegisterPage from './pages/Register';
+import DashboardPage from './pages/Dashboard';
+import ChatPage from './pages/Chat';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<RegisterPage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/dashboard' element={<DashboardPage />}/>
+        <Route path='/chat' element={<ChatPage />}/>
+      </Routes>
+    
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Groupes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GroupesPage() {
+  return (
+    <div>Groupes page</div>
+  )
+}
